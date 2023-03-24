6b9da08577420eee31121f0d23b0aa5781ac7218
cryptohero92 <alwaysflexibleman@gmail.com>
2023-03-24 20:01:11 +0900

    nativebalance getting logic implemented

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,15 @@
+import { useEvmNativeBalance } from "@moralisweb3/next";
+
 function HomePage() {
-    return <div>Welcome to my Next.js dApp!</div>
-  }
-  
-  export default HomePage
+    const address = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
+    const { data: nativeBalance } = useEvmNativeBalance({ address });
+
+    return (
+        <div>
+            <h3>Wallet: {address}</h3>
+            <h3>Native Balance: {nativeBalance?.balance.ether}</h3>
+        </div>
+    );
+}
+
+export default HomePage
